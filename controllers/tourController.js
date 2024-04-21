1c5f664c49ede1e735b717f40e3da9f0556aaec9
José Omar <joseomar_coppola@hotmail.com>
2024-04-21 14:22:23 +0200

    creating a document using moongose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,15 +18,18 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
-exports.checkBody = (req, res, next, val) => {
-  if (req.body.name === null || req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Doesnt have name || price!',
-    });
-  }
-  next();
-};
+
+//now our mongoose model is going to take care of that, it worked to show me
+//how does the middleware worked
+// exports.checkBody = (req, res, next, val) => {
+//   if (req.body.name === null || req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Doesnt have name || price!',
+//     });
+//   }
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -52,14 +55,30 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-      res.status(201).json({
-        status: 'success',
-        // data: {
-        //   tour: newTour,
-        // }
-      });
-    };
+exports.createTour = async (req, res) => {
+
+  //we can do it easier than this :)
+  // const newTour = new Tour({})
+  // newTour.save()
+
+  //creating documents using moongose
+  const newTour = await Tour.create(req.body);
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+        status: 'fail',
+        message: [err,err.message]
+    })
+}
+}
+  
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
